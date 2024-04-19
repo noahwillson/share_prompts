8fc39ca3b9b30b2f96cc77c22ce5e1ca764c3416
noahwillson <nwillson2601@gmail.com>
2024-04-19 02:36:15 +0200

    feat: added routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
